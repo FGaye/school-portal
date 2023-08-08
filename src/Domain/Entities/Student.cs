cc0491bce703093304b2b47b12754a891d95d41c
chamcherno <cherno@writemedia.co.uk>
2023-08-08 15:07:41 +0000

    Added the student entity

---
+++ src/Domain/Entities/Student.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public int MAT_Number { get; set; }
+    }
+}
