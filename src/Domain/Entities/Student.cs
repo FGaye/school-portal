71196b905b53f9ee2c1403b6c3fdbfaca916f1ed
chamcherno <cherno@writemedia.co.uk>
2023-08-08 15:34:56 +0000

    Added more entities to the students, courses, and lecturers.

--- src/Domain/Entities/Student.cs
+++ src/Domain/Entities/Student.cs
@@ -1,14 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Domain.Entities
 {
     public class Student
     {
-        public int Id { get; set; }
-        public string FullName { get; set; }
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
         public int MAT_Number { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public int YearEnrolled { get; set; }
+        public DateTime DateOfGraduation { get; set; }
+        public string GuardianName { get; set; }
+        public string GuardianContact { get; set; }
+        public double GPA { get; set; }
+
     }
 }
